1cfb5d225e4233588ff8f4170b852bf4e6240d8f
kangjy311 <kangjy311@naver.com>
2024-03-14 17:48:05 +0900

    library: signup

--- library/src/pages/SignupPage/SignupPage.jsx
+++ library/src/pages/SignupPage/SignupPage.jsx
@@ -4,15 +4,64 @@ import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
 import RightTopButton from "../../components/RightTopButton/RightTopButton";
 import { useInput } from "../../hooks/useInput";
 import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignupPage(props) {
+    const navigate = useNavigate();
     const [ username, setUsername, userNameChange ] = useInput(); 
     const [ password, setPassword, passwordChange ] = useInput(); 
     const [ checkPassword, setCheckPassword, checkPasswordChange ] = useInput(); 
     const [ name, setName, nameChange ] = useInput(); 
     const [ email, setEmail, emailChange ] = useInput(); 
+    const [ messageGroup, setMessageGroup ] = useState({
+        username: null,
+        password: null,
+        checkPassword: null,
+        name: null,
+        email: null
+    });
+
+    const handleCheckPassword = (e) => {
+        if(!!e.target.value) {
+            setMessageGroup(messageGroup => {
+                return {
+                    ...messageGroup,
+                    checkPassword: {
+                        type: checkPassword === password ? "success" : "error",
+                        text: checkPassword === password ? "" : "비밀번호가 서로 일치하지 않습니다."
+                    }
+                }
+            })
+        } else {
+            setMessageGroup(messageGroup => {
+                return {
+                    ...messageGroup,
+                    checkPassword: null
+                }
+            })
+        }
+    }
 
     const handleSignupSubmit = () => {
+
+        if(messageGroup?.checkPassword?.type === "error") {
+            alert("가입할 회원의 정보를 다시 확인하세요.");
+            return;
+        }
+
+        if(!checkPassword) {
+            setMessageGroup(messageGroup => {
+                return {
+                    ...messageGroup,
+                    checkPassword: {
+                        type: "error",
+                        text: "비밀번호를 입력하세요"
+                    }
+                }
+            })
+        }
+
         const signupData = {
             username,
             password,
@@ -29,9 +78,55 @@ function SignupPage(props) {
         try {
             const response = await axios.post("http://localhost:8080/auth/signup", signupData)
             // axios는 알아서 JSON으로 바꿔준다
-            console.log(response);
+            // axios badrequst를 catch로 보냄
+            console.log(response.data);     //정상정인 응답이면 true
+            if(response.data) {
+                navigate("/auth/signin");
+            }
         } catch(error) {
-            console.log(error);
+            const errorMap = error.response.data;
+            const entries = Object.entries(errorMap);
+            let newMessageGroup = {
+                // 초기화 
+                username: {
+                    type: "success",
+                    text: "사용할 수 있는 사용자이름 입니다"
+                },
+                password: {
+                    type: "success",
+                    text: ""
+                },
+                checkPassword: {
+                    type: "success",
+                    text: ""
+                },
+                name: {
+                    type: "success",
+                    text: ""
+                },
+                email: {
+                    type: "success",
+                    text: ""
+                }
+            };
+            for(let [ key, value ] of entries) {
+                newMessageGroup = {
+                    ...newMessageGroup,
+                    [key]: {
+                        type: "error",
+                        text: value
+                    }
+                }
+            }
+            if(newMessageGroup.password.type === "error") {
+                newMessageGroup = {
+                    ...newMessageGroup,
+                    checkPassword: null
+                }
+                setPassword(() => "");
+                setCheckPassword(() => "");
+            }
+            setMessageGroup(() => newMessageGroup);
         }
     }
     
@@ -41,11 +136,11 @@ function SignupPage(props) {
                 <h1>회원가입</h1> 
                 <RightTopButton onClick={handleSignupSubmit}>가입하기</RightTopButton> 
             </div>
-            <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange}/>
-            <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange}/>
-            <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호 확인"} value={checkPassword} onChange={checkPasswordChange}/>
-            <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange}/>
-            <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange}/>
+            <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange} message={messageGroup.username}/>
+            <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange} message={messageGroup.password}/>
+            <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호 확인"} value={checkPassword} onChange={checkPasswordChange} onBlur={handleCheckPassword} message={messageGroup.checkPassword}/>
+            <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange} message={messageGroup.name}/>
+            <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange} message={messageGroup.email}/>
         </>
     );
 }
